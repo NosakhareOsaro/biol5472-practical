e0032ab6d818d8fbb940c78db911cae457b99d41
Nosakhare Osaro <osaronosakhare@gmail.com>
2026-01-12 13:43:55 +0000

    Add type annotations to existing codebase

--- Sequences/Fasta.py
+++ Sequences/Fasta.py
@@ -1,26 +1,27 @@
-import typing
+from typing import Iterator, Type, Any
+from collections.abc import Iterable
 
 # class to parse a fasta file    
 class FastaFile:
 
     # constructor - only attribute is string containing a file name
-    def __init__(self, file):
-        self._file = file
+    def __init__(self, file: str) -> None:
+        self._file: str = file
 
     # getter for file name
     @property
-    def file(self):
+    def file(self) -> str:
         return self._file
 
     # method to get DNASequence records out from a fasta file
-    # yields so we can iterate all the way over the file
-    # and return a DNASequence for every pair of lines
-    # this function now takes the sequence class as a parameter
-    @typing.no_type_check
-    def get_seq_record(self, sequence_class):
+    # yields sequence objects (DNASequence or similar)
+    def get_seq_record(
+        self,
+        sequence_class: Type[Any],
+    ) -> Iterator[Any]:
         with open(self.file) as filehandle:
             for line in filehandle:
                 if line.startswith('>'):
-                    id = line.rstrip().lstrip('>')
-                    seq = next(filehandle).rstrip()
-                yield sequence_class(id, seq)    
+                    id: str = line.rstrip().lstrip('>')
+                    seq: str = next(filehandle).rstrip()
+                    yield sequence_class(id, seq)
--- Sequences/Sequences.py
+++ Sequences/Sequences.py
@@ -1,101 +1,72 @@
+from collections.abc import Iterator
+
 # create the superclass
 class BioSequence:
 
-    # constructor sets  shared attributes
-    def __init__(self, id, seq):
-        self._id = id
-        self._seq = seq.upper()
+    def __init__(self, id: str, seq: str) -> None:
+        self._id: str = id
+        self._seq: str = seq.upper()
 
-    # getters for shared attributes
-    # getter for id
     @property
-    def id(self):
+    def id(self) -> str:
         return self._id 
 
-    # getter for seq
     @property
-    def seq(self):
+    def seq(self) -> str:
         return self._seq
     
-    # special method shared by subclasses
-    # prints the class attributes nicely
-    def __str__(self):
-        return f'Sequence Object: ID; {self.id}, \
-Seq; {self.seq}'
+    def __str__(self) -> str:
+        return f'Sequence Object: ID; {self.id}, Seq; {self.seq}'
     
-    # returns the length of the sequence when len is called on an object
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.seq)
     
-    # iterates over the sequence if a for loop is run on the class
-    def __iter__(self):
+    # Iterates over characters in the sequence string
+    def __iter__(self) -> Iterator[str]:
         return iter(self.seq)
     
-    # return a fasta string
-    def make_fasta(self):
+    def make_fasta(self) -> str:
         return f'>{self.id}\n{self.seq}\n'
-    
-# create the subclass
-# inherits from BioSequence
+
 class DNASequence(BioSequence):
 
-    # don't need to add any shared content here!
-
-    # these methods are only relevant for DNA sequences
-    # gc content method
-    def calc_gc_content(self, dp=2):
-        c_count = self.seq.count('C')
-        g_count = self.seq.count('G')
-        gc_content = (c_count + g_count) / len(self.seq)
+    def calc_gc_content(self, dp: int = 2) -> float:
+        c_count: int = self.seq.count('C')
+        g_count: int = self.seq.count('G')
+        gc_content: float = (c_count + g_count) / len(self.seq)
         return round(gc_content, dp)
     
-    # translate method
-    def translate_seq(self):
-        # make codon table
-        bases = "tcag".upper()
-        codons = [a + b + c for a in bases for b in bases for c in bases]
-        amino_acids = 'FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG'
-        codon_table = dict(zip(codons, amino_acids))
+    def translate_seq(self) -> str:
+        bases: str = "TCAG"
+        codons: list[str] = [a + b + c for a in bases for b in bases for c in bases]
+        amino_acids: str = 'FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG'
+        codon_table: dict[str, str] = dict(zip(codons, amino_acids))
 
-        # do translation
-        # use a list comprehension and join to make a string
-        aa_list = [codon_table[self.seq[start:start+3]] for start in range(0, len(self.seq) -2, 3)]
-        protein = ''.join(aa_list)
-        return protein
+        aa_list: list[str] = [
+            codon_table[self.seq[start:start+3]]
+            for start in range(0, len(self.seq) - 2, 3)
+        ]
+        return ''.join(aa_list)
         
-    # this is a polymorphic function to get the protein length
-    # // is a floor division
-    def get_protein_len(self):
+    def get_protein_len(self) -> int:
         return len(self.seq) // 3
 
-
-
-# create the subclass
-# inherits from BioSequence
 class ProteinSequence(BioSequence):
 
-    # constructor override
-    # descr is optional, default is an empty string
-    def __init__(self, id, seq, descr=''):
-        # call the constructor from the super class to set shared attributes
+    def __init__(self, id: str, seq: str, descr: str = '') -> None:
         super().__init__(id, seq)
-        # aslo set the unique attribute
-        self._descr = descr
+        self._descr: str = descr
 
-    # getter for descr attribute
     @property
-    def descr(self):
+    def descr(self) -> str:
         return self._descr
 
-    # method specific for protein sequences
-    def get_prop_hydrophobic(self):
-        count_hydrophobic = 0
-        hydrophobic = ['A', 'I', 'L', 'M', 'F', 'W', 'Y', 'V']
+    def get_prop_hydrophobic(self) -> float:
+        count_hydrophobic: int = 0
+        hydrophobic: list[str] = ['A', 'I', 'L', 'M', 'F', 'W', 'Y', 'V']
         for hydro in hydrophobic:
-            count = self.seq.count(hydro)
-            count_hydrophobic += count
+            count_hydrophobic += self.seq.count(hydro)
         return count_hydrophobic / len(self.seq)
     
-    # polymorphic method to get protein length
-    def get_protein_len(self):
+    def get_protein_len(self) -> int:
         return len(self.seq)
